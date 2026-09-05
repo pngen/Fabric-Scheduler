b223c7c1ddf64d891af118d34da20174779cf751
pngen <pngen93@gmail.com>
2026-09-04 20:37:03 -0700

    hygiene: remove probe artifacts from tracking

--- __build_probe.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-cd /d "E:\The Journey\Coding\GitHub\production\Fabric-Scheduler"
-cl /nologo /std:c++20 /EHsc /W4 /MD /I include __probe.cpp /Fe:__probe.exe /link "build\Release\FabricScheduler.lib" ws2_32.lib
-echo CL_EXIT=%ERRORLEVEL%
